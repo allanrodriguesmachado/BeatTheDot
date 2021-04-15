b705a41ede938819bbbd809e0015e04ed3156d10
Allan Rodrigues <allanrodriguesdeveloper@hotmail.com>
2021-04-15 00:20:10 -0300

    Banco de dados

---
+++ src/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
+
+
+/**
+    Pastas do projeto
+ **/
+
+define('MODEL_PATH', realpath(dirname(__FILE__). '/../models'));
+
+
+/**
+    Carregamento banco de dados
+ **/
+require_once (realpath(dirname(__FILE__) . '/DataBase.php'));
