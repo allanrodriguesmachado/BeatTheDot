6b64f4e4208adc7c747367a16cb8bd9f8d49e3ed
allanrodriguesmachado <allan.rodrigues14@hotmail.com>
2021-06-06 16:58:26 -0300

    Usuário do banco

--- public/index.php
+++ public/index.php
@@ -12,5 +12,10 @@ require_once ("../src/config/config.php");
 require_once ("../src/models/User.php");
 
 $user = new User(['name'=>'Allan', 'email'=>'allan@php.com.br']);
-echo $user->getSelect(['id' => 1], 'name, email');
-echo User::getSelect(['name' => 'Allan', 'email' => 'allan@php.com.br']);
+print_r(User::get(['id' => 1], 'name, email'));
+
+echo '<br>';
+foreach (User::get([], 'name') as $user){
+    echo $user->name;
+    echo '<br>';
+}
