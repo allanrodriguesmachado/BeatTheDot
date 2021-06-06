8af22be9e11cbe943bf1751505344d9d46333143
allanrodriguesmachado <allan.rodrigues14@hotmail.com>
2021-06-06 17:29:49 -0300

    Tela de autenticação - LOGIN

--- public/index.php
+++ public/index.php
@@ -9,13 +9,5 @@ require_once ("../vendor/autoload.php");
  * Config
  */
 require_once ("../src/config/config.php");
-require_once ("../src/models/User.php");
+require_once ("../src/views/login.php");
 
-$user = new User(['name'=>'Allan', 'email'=>'allan@php.com.br']);
-print_r(User::get(['id' => 1], 'name, email'));
-
-echo '<br>';
-foreach (User::get([], 'name') as $user){
-    echo $user->name;
-    echo '<br>';
-}
