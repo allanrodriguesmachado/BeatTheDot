002a8fd19eac09c8c48e479ca68f303a7288de59
allanrodriguesmachado <allan.rodrigues14@hotmail.com>
2021-08-08 14:25:40 -0300

    Gerando Comando Select

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require_once(dirname(__FILE__, 2) . '/app/Config/config.php');
 require_once(dirname(__FILE__, 2) . '/app/models/User.php');
 
 $user = new User(['name' => 'Allan', 'email' => 'allan@php.com.br']);
-//print_r($user);
-//echo 'fim';
-print_r($user);
-$user->email = 'rodrigues@php.com.br';
-print_r($user->email);
+
+
+echo User::getSelect(['name' => 1], 'name, email');
+echo '<br>';
+echo User::getSelect(['name' => 'Allan', 'email' => 'allan@php.com.br']);
