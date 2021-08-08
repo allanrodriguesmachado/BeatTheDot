a9cc67d956869424cf01e0399c1a1114c5987f35
allanrodriguesmachado <allan.rodrigues14@hotmail.com>
2021-08-08 13:53:43 -0300

    Classe User 2

--- public/index.php
+++ public/index.php
@@ -4,5 +4,8 @@ require_once(dirname(__FILE__, 2) . '/app/Config/config.php');
 require_once(dirname(__FILE__, 2) . '/app/models/User.php');
 
 $user = new User(['name' => 'Allan', 'email' => 'allan@php.com.br']);
+//print_r($user);
+//echo 'fim';
 print_r($user);
-echo 'fim';
+$user->email = 'rodrigues@php.com.br';
+print_r($user->email);
